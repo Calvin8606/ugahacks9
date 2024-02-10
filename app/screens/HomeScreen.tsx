89c12f866d9432a1462dbcab8ef49e23a5eb7c89
Calvin8606 <calvin71203@gmail.com>
2024-02-10 14:46:53 -0500

    added log in form

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,37 +1,32 @@
-import React, { useEffect } from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-import { Deposit, User } from "../App";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import axios from "axios";
+import React, { useEffect } from 'react';
+import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
+import { Deposit, User } from '../App';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
 // @ts-ignore
-import { EXPO_PUBLIC_BACKEND_URL } from "@env";
+import { EXPO_PUBLIC_BACKEND_URL } from '@env';
 
-interface props {
+interface Props {
   user: User;
   setUserId: Function;
   setUser: Function;
 }
 
-export const HomeScreen = ({ user, setUserId, setUser }: props) => {
+export const HomeScreen = ({ user, setUserId, setUser }: Props) => {
   useEffect(() => {
     if (user.account) {
       axios
         .get(
-          `http://${
-            EXPO_PUBLIC_BACKEND_URL || process.env.EXPO_PUBLIC_BACKEND_URL
-          }/nessie?url=http://api.nessieisreal.com/accounts/${
-            user?.account._id
-          }/deposits/?key=bf8433e4df1dc693db643a4926845cbb`
+          `http://${EXPO_PUBLIC_BACKEND_URL || process.env.EXPO_PUBLIC_BACKEND_URL}/nessie?url=http://api.nessieisreal.com/accounts/${user?.account._id}/deposits/?key=bf8433e4df1dc693db643a4926845cbb`
         )
         .then((res: any) => {
-          console.log("HEEE", res);
           setUser((prev: User) => ({
-            ...(prev as User),
+            ...prev,
             deposits: res.data as Deposit[],
           }));
         });
     }
-  }, []);
+  }, [user.account]);
 
   return (
     <View style={styles.container}>
@@ -39,12 +34,22 @@ export const HomeScreen = ({ user, setUserId, setUser }: props) => {
       {user.account && (
         <Text style={styles.balanceText}>Balance ${user.account.balance}</Text>
       )}
+      <Text style={styles.transactionsHeader}>Transactions</Text>
+      <FlatList
+        data={user.deposits}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => (
+          <View style={styles.transactionRow}>
+            <Text style={styles.transactionText}>{item.transaction_date}: ${item.amount}</Text>
+          </View>
+        )}
+      />
       <View style={styles.buttonContainer}>
         <Button
           title="Log out"
           onPress={() => {
-            AsyncStorage.removeItem("@storage_Key");
-            setUserId("");
+            AsyncStorage.removeItem('@storage_Key');
+            setUserId('');
           }}
         />
       </View>
@@ -55,20 +60,33 @@ export const HomeScreen = ({ user, setUserId, setUser }: props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
     paddingTop: 50,
+    paddingHorizontal: 10,
   },
   balanceText: {
     fontSize: 24,
     marginBottom: 20,
   },
+  transactionsHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  transactionRow: {
+    paddingVertical: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  transactionText: {
+    fontSize: 16,
+  },
   buttonContainer: {
-    position: 'absolute', // Position the button absolutely
-    bottom: 50, // Distance from the bottom
-    left: 0, // Start from the left edge
-    right: 0, // Stretch to the right edge
-    alignItems: 'center', // Center the button horizontally
+    position: 'absolute',
+    bottom: 50,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
   },
 });
 
