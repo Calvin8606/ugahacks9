b2d0d7d802ac2282e1d7005ce3f093fb156f6d81
EHansonn <evanhanson02@gmail.com>
2024-02-10 16:06:57 -0500

    feat: added withdraws and deposits

--- app/App.tsx
+++ app/App.tsx
@@ -1,17 +1,27 @@
 // App.js
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import AddFundsScreen from "./screens/AddFundsScreen";
 import RemoveFundsScreen from "./screens/RemoveFundsScreen";
-import { View, TextInput, Button, StyleSheet, Alert } from "react-native";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Alert,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import { SignUpForm } from "./screens/SignUpScreen";
 import { LoginForm } from "./screens/LogInScreen";
 // @ts-ignore
 import { EXPO_PUBLIC_BACKEND_URL } from "@env";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import DepositScreen from "./screens/DepositScreen";
+import WithdrawScreen from "./screens/WithdrawScreen";
 const Stack = createNativeStackNavigator();
 export interface Address {
   street_number: string;
@@ -177,8 +187,10 @@ export default function App() {
         console.error("Error:", error.message); // handle the error
       });
   }, [userId]);
+  const navigationRef = useRef() as React.MutableRefObject<any>;
+  const [menuVisible, setMenuVisible] = useState(false);
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home">
           {(props) => {
@@ -195,7 +207,79 @@ export default function App() {
         <Stack.Screen name="Remove Funds">
           {(props) => <RemoveFundsScreen {...props} setBalance={setBalance} />}
         </Stack.Screen>
+        <Stack.Screen name="Deposit">
+          {(props) => {
+            return user?.account ? (
+              <DepositScreen {...props} user={user} />
+            ) : (
+              <SignUpForm setUserId={setUser} userId={userId}></SignUpForm>
+            );
+          }}
+        </Stack.Screen>
+        <Stack.Screen name="Withdraw">
+          {(props) => {
+            return user?.account ? (
+              <WithdrawScreen {...props} user={user} />
+            ) : (
+              <SignUpForm setUserId={setUser} userId={userId}></SignUpForm>
+            );
+          }}
+        </Stack.Screen>
       </Stack.Navigator>
+      <View style={styles.menuContainer}>
+        <TouchableOpacity
+          style={styles.menuButton}
+          onPress={() => setMenuVisible(!menuVisible)}
+        >
+          <Text style={styles.menuButtonText}>Menu</Text>
+        </TouchableOpacity>
+        {menuVisible && (
+          <View style={styles.menu}>
+            <Button
+              title="Deposit"
+              onPress={() => {
+                // Navigation code for Add Funds
+                navigationRef.current?.navigate("Deposit");
+              }}
+            />
+            <Button
+              title="Withdraw"
+              onPress={() => {
+                navigationRef.current?.navigate("Withdraw");
+                // Navigation code for Remove Funds
+              }}
+            />
+            <Button
+              title="Home"
+              onPress={() => {
+                navigationRef.current?.navigate("Home");
+                // Navigation code for Remove Funds
+              }}
+            />
+          </View>
+        )}
+      </View>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  menuContainer: {
+    position: "absolute",
+    bottom: 20,
+    left: 10,
+    zIndex: 1,
+  },
+  menuButton: {
+    backgroundColor: "#007bff",
+    padding: 10,
+    borderRadius: 20,
+  },
+  menuButtonText: {
+    color: "#ffffff",
+    textAlign: "center",
+  },
+  menu: {
+    marginTop: 5,
+  },
+});
---
+++ app/screens/WithdrawScreen.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { View, TextInput, Button, StyleSheet } from "react-native";
+import { User } from "../App";
+import axios from "axios";
+// @ts-ignore
+import { EXPO_PUBLIC_BACKEND_URL } from "@env";
+interface Props {
+  user: User;
+  setUser?: Function;
+}
+
+export const WithdrawScreen = ({ user, setUser }: Props) => {
+  const [amount, setAmount] = useState("");
+  function getCurrentDateFormatted(): string {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = (now.getMonth() + 1).toString().padStart(2, "0"); // JS months are 0-based
+    const day = now.getDate().toString().padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }
+  const handleAddFunds = () => {
+    const customHeaders2 = {
+      "X-Custom-Header": `http://api.nessieisreal.com/accounts/${user?.account?._id}/withdrawals?key=bf8433e4df1dc693db643a4926845cbb`,
+    };
+    const body = {
+      medium: "balance",
+      transaction_date: getCurrentDateFormatted().toString(),
+      status: "pending",
+      description: "a balance",
+      amount: parseInt(amount),
+    };
+    console.log(body, customHeaders2);
+    axios
+      .post(
+        `http://${
+          EXPO_PUBLIC_BACKEND_URL || process.env.EXPO_PUBLIC_BACKEND_URL
+        }/nessie?url=http://api.nessieisreal.com/accounts/${
+          user?.account?._id
+        }/withdrawals?key=bf8433e4df1dc693db643a4926845cbb`,
+        body,
+        {
+          headers: customHeaders2,
+        }
+      )
+      .then((res: any) => {
+        console.log("eeee69", res);
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setAmount}
+        value={amount}
+        keyboardType="numeric"
+        placeholder="Enter amount"
+      />
+      <Button title="Withdraw Funds" onPress={handleAddFunds} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+  },
+});
+
+export default WithdrawScreen;
